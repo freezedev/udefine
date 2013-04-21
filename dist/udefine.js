343613f393bde81953fb65851513c0bff4172504
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-21 14:21:25 +0200

    udefine definition is now more independant

--- dist/udefine.js
+++ dist/udefine.js
@@ -1,34 +1,39 @@
 (function() {
   'use strict';
-  var hasModule, udefine;
+  var hasModule, _ref;
+
+  Array.isArray || (Array.isArray = function(a) {
+    return a.push === Array.prototype.push && (a.length != null);
+  });
 
   hasModule = (typeof module !== "undefined" && module !== null) && module.exports;
 
-  udefine = function(name, deps, factory) {
-    var _ref, _ref1;
+  (function(root) {
+    root.udefine = function(name, deps, factory) {
+      var _ref1, _ref2;
 
-    if (Array.isArray(name)) {
-      _ref = [void 0, name, deps], name = _ref[0], deps = _ref[1], factory = _ref[2];
-    } else {
-      if (typeof name === 'function') {
-        _ref1 = [void 0, void 0, name], name = _ref1[0], deps = _ref1[1], factory = _ref1[2];
-      }
-    }
-    return (function(factory) {
-      if (typeof define !== "undefined" && define !== null) {
-        if (define.amd || define.umd) {
-          return define.apply(this, arguments);
-        }
+      if (Array.isArray(name)) {
+        _ref1 = [void 0, name, deps], name = _ref1[0], deps = _ref1[1], factory = _ref1[2];
       } else {
-        if (hasModule) {
-
-        } else {
-
+        if (typeof name === 'function') {
+          _ref2 = [void 0, void 0, name], name = _ref2[0], deps = _ref2[1], factory = _ref2[2];
         }
       }
-    })(factory);
-  };
+      return (function(factory) {
+        if (typeof define !== "undefined" && define !== null) {
+          if (define.amd || define.umd) {
+            return define.apply(this, arguments);
+          }
+        } else {
+          if (hasModule) {
 
-  udefine.globals = {};
+          } else {
+
+          }
+        }
+      })(factory);
+    };
+    return root.udefine.globals = {};
+  })((_ref = typeof module !== "undefined" && module !== null ? module.exports : void 0) != null ? _ref : this);
 
 }).call(this);
