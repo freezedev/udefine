0fa7a4c1c87a5ecb66d06a2cd669121046cebfd8
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-21 14:03:58 +0200

    Updated dist JS

---
+++ dist/udefine.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+  var hasModule, udefine;
+
+  hasModule = (typeof module !== "undefined" && module !== null) && module.exports;
+
+  udefine = function() {};
+
+  udefine.globals = {};
+
+}).call(this);
